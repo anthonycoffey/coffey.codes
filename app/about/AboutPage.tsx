4c5d774d6b9265bfe8464aa1b8925c599819b6d5
Anthony Coffey <coffey.j.anthony@gmail.com>
2023-10-30 14:39:18 -0500

    ...

--- app/about/AboutPage.tsx
+++ app/about/AboutPage.tsx
@@ -1,58 +1,100 @@
-import Chip from "../../components/Chip";
-
+const LOGOS = [
+  "angular-icon.svg",
+  "aws-amplify.svg",
+  "aws-lambda.svg",
+  "aws.svg",
+  "css-3.svg",
+  "expo.svg",
+  "firebase.svg",
+  "google-cloud-functions.svg",
+  "google-cloud-run.svg",
+  "google-cloud.svg",
+  "graphql.svg",
+  "heroku.svg",
+  "highcharts.svg",
+  "html-5.svg",
+  "javascript.svg",
+  "mongodb.svg",
+  "mysql.svg",
+  "netlify.svg",
+  "nodejs.svg",
+  "php.svg",
+  "postgresql.svg",
+  "react.svg",
+  "sass.svg",
+  "sentry.svg",
+  "swagger.svg",
+  "tailwindcss.svg",
+  "typescript-icon.svg",
+  "vercel.svg",
+  "vue.svg",
+  "wordpress.svg",
+];
 export default function AboutPage() {
   return (
-    <div className="container mx-auto flex grid grid-cols-1 md:grid-cols-2 mt-10">
-      <div className="prose lg:prose-xl mb-10 px-4 text-center md:text-left mx-auto">
-        <h1 className="text-2xl font-bold mb-6">👋 Hey Y'all</h1>
-        <p className="leading-1 tracking-wide">
-          My name is <b>Anthony Coffey</b> and I'm an <b>Artist</b>,{" "}
-          <b>Musician</b> and <b>Software Engineer</b> living in{" "}
-          <b>Austin, Texas</b>.
-        </p>
-        <p className="leading-1 tracking-wide">
-          In May of 2012, I graduated from Gulf Coast State College and started
-          a business doing WordPress plugin and theme development.
-        </p>
-        <p className="leading-1 tracking-wide">
-          I was able to launch my career right out of college thanks to some
-          great mentors and the Elance platform, which was later acquired by
-          Upwork.
-        </p>
-        <p className="leading-1 tracking-wide">
-          Since then, I've gained valuable professional experience working as a
-          sysadmin, engineer, and consultant for start-ups and small to
-          medium-sized businesses.
-        </p>
-        <p className="leading-1 tracking-wide">
-          March 2022, I was employed as a Full Stack Software Engineer at{" "}
-          <a
-            href="https://www.maranihealth.com/"
-            target="_blank"
-            rel="noopener noreferrer"
-            className="text-blue-500 hover:underline focus:underline focus:outline-none"
-          >
-            Marani Health
-          </a>
-        </p>
-        <p className="leading-1 tracking-wide">
-          If you have a project you'd like to discuss, please feel free to{" "}
-          <a
-            href="/contact"
-            className="text-blue-500 hover:underline focus:underline focus:outline-none"
-          >
-            contact me
-          </a>
-          .
-        </p>
+    <>
+      <div className="container mx-auto flex grid grid-cols-1 md:grid-cols-2 mt-10">
+        <div className="prose lg:prose-xl mb-10 px-4 text-center md:text-left mx-auto">
+          <h1 className="text-2xl font-bold mb-6">👋 Hey Y'all</h1>
+          <p className="leading-1 tracking-wide">
+            My name is <b>Anthony Coffey</b> and I'm an <b>Artist</b>,{" "}
+            <b>Musician</b> and <b>Software Engineer</b> living in{" "}
+            <b>Austin, Texas</b>.
+          </p>
+          <p className="leading-1 tracking-wide">
+            In May of 2012, I graduated from Gulf Coast State College and
+            started a business doing WordPress plugin and theme development.
+          </p>
+          <p className="leading-1 tracking-wide">
+            I was able to launch my career right out of college thanks to some
+            great mentors and the Elance platform, which was later acquired by
+            Upwork.
+          </p>
+          <p className="leading-1 tracking-wide">
+            Since then, I've gained valuable professional experience working as
+            a sysadmin, engineer, and consultant for start-ups and small to
+            medium-sized businesses.
+          </p>
+          <p className="leading-1 tracking-wide">
+            March 2022, I was employed as a Full Stack Software Engineer at{" "}
+            <a
+              href="https://www.maranihealth.com/"
+              target="_blank"
+              rel="noopener noreferrer"
+              className="text-blue-500 hover:underline focus:underline focus:outline-none"
+            >
+              Marani Health
+            </a>
+          </p>
+          <p className="leading-1 tracking-wide">
+            If you have a project you'd like to discuss, please feel free to{" "}
+            <a
+              href="/contact"
+              className="text-blue-500 hover:underline focus:underline focus:outline-none"
+            >
+              contact me
+            </a>
+            .
+          </p>
+        </div>
+        <div className="flex justify-center items-center content-center">
+          <img
+            src="/psych.png"
+            alt="Contact"
+            className="rounded-full p-10 md:p-5 mx-auto"
+          />
+        </div>
       </div>
-      <div className="flex justify-center items-center content-center">
-        <img
-          src="/psych.png"
-          alt="Contact"
-          className="rounded-full p-10 md:p-5 mx-auto"
-        />
+      <div className="container mx-auto flex  mt-10">
+        <div className="mb-10 px-4 text-center md:text-left mx-auto">
+          <h4>I have notable experience with the following technologies:</h4>
+          <div className="grid grid-cols-12 gap-4 justify-items-center items-center content-center">
+            {LOGOS.map((logo) => (
+              <img src={`/logos/${logo}`} />
+            ))}
+          </div>
+        </div>
       </div>
-    </div>
+    </>
   );
 }
--- app/about/page.tsx
+++ app/about/page.tsx
@@ -1,2 +1,5 @@
+import fs from "fs";
+import path from "path";
+
 import AboutPage from "./AboutPage";
 export default AboutPage;

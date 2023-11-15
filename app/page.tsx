8d2132167b9ae885572ee51e074d04528d3cf5c9
Anthony Coffey <coffey.j.anthony@gmail.com>
2023-11-14 21:44:54 -0600

    ..

--- app/page.tsx
+++ app/page.tsx
@@ -4,8 +4,6 @@ import HeroSection from "../components/HeroSection";
 export default async function Page() {
   return (
     <>
-      {/*<HeroSection />*/}
-
       <section className="container mx-auto">
         <div className="flex justify-center">
           <img src="/underconstruction-bar.gif" alt="under construction" />

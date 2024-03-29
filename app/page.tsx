b8fc5caa19135376e1c2b643f1ee09c3db98144f
Anthony Coffey <coffey.j.anthony@gmail.com>
2024-03-29 06:36:23 -0500

    removing under contruction GIFs, and minor changes to about page

--- app/page.tsx
+++ app/page.tsx
@@ -4,11 +4,6 @@ export default async function Page() {
   return (
     <>
       <section className="container mx-auto">
-        <div className="flex justify-center">
-          <img src="/underconstruction-bar.gif" alt="under construction" />
-          <img src="/underconstruction-bar.gif" alt="under construction" />
-          <img src="/underconstruction-bar.gif" alt="under construction" />
-        </div>
         <AllArticles />
       </section>
     </>

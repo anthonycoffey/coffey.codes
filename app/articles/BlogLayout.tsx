a310d496d75ecc5d12eca9cdb1d3a3e2761e9563
Anthony Coffey <coffey.j.anthony@gmail.com>
2024-04-01 05:11:39 -0500

    article update, moving gradient generation function to `utils` folder

--- app/articles/BlogLayout.tsx
+++ app/articles/BlogLayout.tsx
@@ -1,30 +1,7 @@
 import ArticleInfo from "components/ArticleInfo";
 import { headers } from "next/headers";
 import { getArticleBySlug } from "utils/getArticles";
-
-const generateRandomGradient = (seed) => {
-  const colorPairs = [
-    ["#f6d365", "#fda085"], // Pastel yellow to pastel orange
-    ["#fbc2eb", "#a6c1ee"], // Pastel pink to pastel blue
-    ["#e0c3fc", "#8ec5fc"], // Pastel purple to pastel sky blue
-    ["#ffecd2", "#fcb69f"], // Pastel peach to pastel orange
-    ["#a1c4fd", "#c2e9fb"], // Pastel blue to pastel cyan
-    ["#fed6e3", "#a8edea"], // Pastel pink to pastel aqua
-  ];
-
-  // Ensure the seed is a string
-  const validSeed = typeof seed === "string" ? seed : "";
-
-  // Simple hash function to generate a pseudo-random number based on the seed
-  const hash = Array.from(validSeed).reduce(
-    (acc, char) => acc + char.charCodeAt(0),
-    0,
-  );
-
-  const index = hash % colorPairs.length;
-  const [color1, color2] = colorPairs[index];
-  return `radial-gradient(circle at 50% 50%, ${color1}, ${color2})`;
-};
+import generateRandomGradient from "utils/generateRandomGradient";
 
 export default async function Layout({
   children,

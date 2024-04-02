e70eb7c4cb00b736dda4f9ed3bf82fea415ea1e4
Anthony Coffey <coffey.j.anthony@gmail.com>
2024-04-02 17:54:33 -0500

    updating post slug: console-log-nested-object-in-javascript

--- utils/colorPairs.ts
+++ utils/colorPairs.ts
@@ -1,41 +1,22 @@
 const colorPairs: string[][] = [
-  // Vibrant and Energetic
-  ["#FF6347", "#FF4500"], // Tomato to Orange Red
-  ["#00BFFF", "#1E90FF"], // Deep Sky Blue to Dodger Blue
-  ["#BA55D3", "#9400D3"], // Medium Orchid to Dark Violet
-  ["#87CEFA", "#B0C4DE"], // Light Sky Blue to Light Steel Blue
-  ["#4682B4", "#708090"], // Steel Blue to Slate Gray
-  ["#00CED1", "#20B2AA"], // Dark Turquoise to Light Sea Green
-  ["#5F9EA0", "#66CDAA"], // Cadet Blue to Medium Aquamarine
-  ["#AFEEEE", "#E0FFFF"], // Pale Turquoise to Light Cyan
-  ["#FF69B4", "#FF1493"], // Hot Pink to Deep Pink
-  ["#DB7093", "#C71585"], // Pale Violet Red to Medium Violet Red
-  ["#FFA07A", "#FA8072"], // Light Salmon to Salmon
-  ["#FF7F50", "#FF6347"], // Coral to Tomato
-  ["#FF4500", "#DC143C"], // Orange Red to Crimson
-  ["#00FA9A", "#00FF7F"], // Medium Spring Green to Spring Green
-  ["#00FFFF", "#00CED1"], // Aqua to Dark Turquoise
-  ["#8A2BE2", "#4B0082"], // Blue Violet to Indigo
-  ["#BA55D3", "#800080"], // Medium Orchid to Purple
-  ["#DDA0DD", "#DA70D6"], // Plum to Orchid
-  ["#EE82EE", "#FF00FF"], // Violet to Magenta
-  ["#FF00FF", "#FF69B4"], // Magenta to Hot Pink
-  ["#FF1493", "#FF4500"], // Deep Pink to Orange Red
-  ["#FFA07A", "#FFD700"], // Light Salmon to Gold
-  ["#32CD32", "#00BFFF"], // Lime Green to Deep Sky Blue
-  ["#BA55D3", "#87CEFA"], // Medium Orchid to Light Sky Blue
-  ["#4682B4", "#00CED1"], // Steel Blue to Dark Turquoise
-  ["#5F9EA0", "#AFEEEE"], // Cadet Blue to Pale Turquoise
-  ["#FF6347", "#FFD700"], // Tomato to Gold
-  ["#9400D3", "#87CEFA"], // Dark Violet to Light Sky Blue
-  ["#708090", "#00CED1"], // Slate Gray to Dark Turquoise
-  ["#66CDAA", "#AFEEEE"], // Medium Aquamarine to Pale Turquoise
-  ["#FF4500", "#FF69B4"], // Orange Red to Hot Pink
-  ["#800080", "#1E90FF"], // Purple to Dodger Blue
-  ["#DA70D6", "#4169E1"], // Orchid to Royal Blue
-  ["#FF00FF", "#8A2BE2"], // Magenta to Blue Violet
-  ["#FF69B4", "#DDA0DD"], // Hot Pink to Plum
-  ["#FF1493", "#EE82EE"], // Deep Pink to Violet
+  ["#8EC5FC", "#E0C3FC"], // Light Blue to Light Purple
+  ["#6A82FB", "#FC5C7D"], // Indigo Blue to Red Pink
+  ["#B6FBFF", "#83A4D4"], // Light Cyan to Dusty Blue
+  ["#67B26F", "#4ca2cd"], // Fresh Green to Cool Blue
+  ["#FF758C", "#FF7EB3"], // Pale Red to Soft Pink
+  ["#22E1FF", "#1D8FE1"], // Electric Blue to Blue
+  ["#B721FF", "#21D4FD"], // Vivid Purple to Bright Turquoise
+  ["#43E97B", "#38F9D7"], // Green to Turquoise
+  ["#FF4E50", "#F9D423"], // Red to Yellow
+  ["#16BFFD", "#CB3066"], // Sky Blue to Pink
+  ["#00C9FF", "#92FE9D"], // Bright Blue to Light Green
+  ["#74EBD5", "#ACB6E5"], // Turquoise to Lavender Blue
+  ["#FF7E5F", "#FEB47B"], // Coral to Peach
+  ["#FBD3E9", "#BB377D"], // Pink Lavender to Dark Pink
+  ["#ABDCFF", "#0396FF"], // Pale Blue to Blue
+  ["#FEB692", "#EA5455"], // Peachy Pink to Red
+  ["#CE9FFC", "#7367F0"], // Lavender to Purple
+  ["#90F7EC", "#32CCBC"], // Light Turquoise to Greenish Blue
 ];
 
 export default colorPairs;

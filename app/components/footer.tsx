323cec5fb69cdc7a03789f834669d040714f8b66
Anthony Coffey <coffey.j.anthony@gmail.com>
2024-10-13 09:53:42 -0500

    ...

--- app/components/footer.tsx
+++ app/components/footer.tsx
@@ -50,7 +50,7 @@ export default function Footer() {
   ];
 
   return (
-    <footer className="mb-16">
+    <footer className="my-16">
       <ul className="font-sm mt-8 flex flex-col space-x-0 space-y-2 md:flex-row md:space-x-4 md:space-y-0 text-neutral-100">
         {links.map((link, index) => (
           <li key={index}>

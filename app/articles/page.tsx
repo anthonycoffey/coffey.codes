85a852f2cc5e39f19389a61cc8c859fb32041827
Anthony Coffey <coffey.j.anthony@gmail.com>
2024-10-23 18:36:01 -0500

    ...

--- app/articles/page.tsx
+++ app/articles/page.tsx
@@ -1,6 +1,7 @@
 import { BlogPosts } from 'app/components/posts';
 import { getBlogPosts } from 'app/articles/utils';
 import { DocumentTextIcon } from '@heroicons/react/20/solid';
+import { Fragment } from 'react';
 
 export const metadata = {
   title: 'Articles',
@@ -19,6 +20,10 @@ export default async function Page({ searchParams }) {
       <h1 className="font-semibold text-2xl lg:text-3xl tracking-tighter">
         <DocumentTextIcon className="w-6 h-6 inline mr-2" /> Articles
       </h1>
+      <p className="text-neutral-200 mb-4">
+        Welcome to my blog! Discover articles on software engineering, systems
+        design and product management.
+      </p>
       <BlogPosts allBlogs={allBlogs} />
     </section>
   );

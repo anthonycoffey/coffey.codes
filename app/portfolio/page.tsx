d09949eee0a8e7d97ce5309abfb27074c9ad432f
Anthony Coffey <coffey.j.anthony@gmail.com>
2024-12-23 07:23:51 -0600

    ...

--- app/portfolio/page.tsx
+++ app/portfolio/page.tsx
@@ -1,13 +1,22 @@
+import { BriefcaseIcon } from '@heroicons/react/20/solid';
+
 export const metadata = {
-  title: 'Work Portfolio',
+  title: 'Portfolio',
   description:
-    'Work portfolio of current and past projects completed by Anthony Coffey, SWE',
+    ' Explore a selection of my recent web and mobile software projects.',
 };
 
 export default async function Page({ searchParams }) {
   return (
     <section>
-      <div className="page-content"></div>
+      <div className="page-content">
+        <h1 className="font-semibold text-2xl lg:text-3xl tracking-tighter">
+          <BriefcaseIcon className="w-6 h-6 inline mr-2" /> Portfolio
+        </h1>
+        <span className="text-neutral-200">
+          Explore a selection of my recent web and mobile software projects.
+        </span>
+      </div>
     </section>
   );
 }

50798d73fe815d96889f3117587a71b7947ae9ac
Anthony Coffey <coffey.j.anthony@gmail.com>
2025-01-26 23:30:48 -0600

    ...

--- app/page.tsx
+++ app/page.tsx
@@ -55,7 +55,7 @@ export default function Page() {
 
         <div className="flex flex-col justify-center bg-gray-800 p-6 md:p-10 rounded-lg items-center my-6 md:my-10">
           <p className="text-xl md:text-2xl font-bold mb-4 text-center">
-            Hiring Engineers? Let's Chat!
+            Hiring? Let's Chat!
           </p>
           <div className="flex flex-col justify-between w-full space-y-4">
             <a

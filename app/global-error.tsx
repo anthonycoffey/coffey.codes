358dfd9942d39a3627110c79d70233106c7f1290
Anthony Coffey <coffey.j.anthony@gmail.com>
2025-03-02 15:54:13 -0600

    ...

--- app/global-error.tsx
+++ app/global-error.tsx
@@ -1,14 +1,7 @@
-"use client";
+'use client';
+import NextError from 'next/error';
 
-import * as Sentry from "@sentry/nextjs";
-import NextError from "next/error";
-import { useEffect } from "react";
-
-export default function GlobalError({ error }: { error: Error & { digest?: string } }) {
-  useEffect(() => {
-    Sentry.captureException(error);
-  }, [error]);
-
+export default function GlobalError() {
   return (
     <html>
       <body>

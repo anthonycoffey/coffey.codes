8cccac900cab19eb03f274d6498362eee795b61a
Anthony Coffey <coffey.j.anthony@gmail.com>
2025-03-08 01:09:02 -0600

    animate when in view

--- app/components/LogoGrid.tsx
+++ app/components/LogoGrid.tsx
@@ -1,19 +1,22 @@
 'use client';
-import { ReactElement } from 'react';
+import { ReactElement, useRef } from 'react';
 import Image from 'next/image';
-import { motion } from 'framer-motion';
+import { motion, useInView } from 'framer-motion';
 
 type Props = {
   logos: string[];
 };
 
 export default function LogoGrid({ logos }: Props): ReactElement {
+  const ref = useRef(null);
+  const isInView = useInView(ref, { once: true, amount: 0.2 });
+
   const container = {
     hidden: { opacity: 0 },
     show: {
       opacity: 1,
       transition: {
-        staggerChildren: 0.18,
+        staggerChildren: 0.1,
       },
     },
   };
@@ -25,10 +28,11 @@ export default function LogoGrid({ logos }: Props): ReactElement {
 
   return (
     <motion.div
+      ref={ref}
       className="grid grid-cols-6 gap-12 logo-grid max-w-3xl mx-auto"
       variants={container}
       initial="hidden"
-      animate="show"
+      animate={isInView ? 'show' : 'hidden'}
     >
       {logos.map((logo: string) => (
         <motion.div key={logo} variants={item}>

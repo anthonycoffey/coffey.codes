a18bb97cf15ae6561edbfefebd4c48902ed57f2c
Anthony Coffey <coffey.j.anthony@gmail.com>
2025-03-07 22:26:47 -0600

    pagination fixed.

--- app/components/posts.tsx
+++ app/components/posts.tsx
@@ -6,25 +6,27 @@ export function BlogPosts({ allBlogs }) {
   const { currentPage, totalPages } = allBlogs.pagination;
 
   return (
-    <div className="space-y-8">
-      {allBlogs.posts.map((post) => (
-        <Link
-          key={post.slug}
-          className="block p-6 mb-4 bg-white rounded-lg shadow-md hover:bg-gray-100 transition duration-300"
-          href={`/articles/${post.slug}`}
-        >
-          <div className="w-full flex flex-col space-y-2">
-            <p className="text-2xl font-bold text-gray-900">
-              {post.metadata.title}
-            </p>
-            <p className="text-sm text-gray-500">
-              {formatDate(post.metadata.publishedAt, false)}
-            </p>
-            <p className="text-base text-gray-700">{post.metadata.summary}</p>
-          </div>
-        </Link>
-      ))}
+    <>
+      <div className="space-y-8">
+        {allBlogs.posts.map((post) => (
+          <Link
+            key={post.slug}
+            className="block p-6 mb-4 bg-white rounded-lg shadow-md hover:bg-gray-100 transition duration-300"
+            href={`/articles/${post.slug}`}
+          >
+            <div className="w-full flex flex-col space-y-2">
+              <p className="text-2xl font-bold text-gray-900">
+                {post.metadata.title}
+              </p>
+              <p className="text-sm text-gray-500">
+                {formatDate(post.metadata.publishedAt, false)}
+              </p>
+              <p className="text-base text-gray-700">{post.metadata.summary}</p>
+            </div>
+          </Link>
+        ))}
+      </div>
       <Pagination currentPage={currentPage} totalPages={totalPages} />
-    </div>
+    </>
   );
 }

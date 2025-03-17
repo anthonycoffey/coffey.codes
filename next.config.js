bc96eac3b3357393c16f2b653ea3787d8380cf2e
Anthony Coffey <coffey.j.anthony@gmail.com>
2025-03-17 17:35:01 -0500

    removing shit article

--- next.config.js
+++ next.config.js
@@ -4,42 +4,47 @@ const nextConfig = {
   async redirects() {
     return [
       {
-        source: '/articles/console-log-nested-object-in-javascript',
-        destination:
-          '/articles/unveiling-nested-objects-enhanced-console-logging-node-js',
-        permanent: true,
+      source: '/articles/console-log-nested-object-in-javascript',
+      destination:
+        '/articles/unveiling-nested-objects-enhanced-console-logging-node-js',
+      permanent: true,
       },
       {
-        source: '/articles/gradient-background-for-post-thumbnails',
-        destination:
-          '/articles/using-css-gradients-for-dynamic-post-thumbnails',
-        permanent: true,
+      source: '/articles/gradient-background-for-post-thumbnails',
+      destination:
+        '/articles/using-css-gradients-for-dynamic-post-thumbnails',
+      permanent: true,
       },
       {
-        source:
-          '/articles/unveiling-nested-objects-enhanced-console-logging-node-js',
-        destination: '/articles/logging-deep-nested-objects-in-nodejs',
-        permanent: true,
+      source:
+        '/articles/unveiling-nested-objects-enhanced-console-logging-node-js',
+      destination: '/articles/logging-deep-nested-objects-in-nodejs',
+      permanent: true,
       },
       {
-        source: '/about',
-        destination: '/',
-        permanent: true,
+      source: '/about',
+      destination: '/',
+      permanent: true,
       },
       {
-        source: '/articles/my-markdown-resume',
-        destination: '/',
-        permanent: true,
+      source: '/articles/my-markdown-resume',
+      destination: '/articles',
+      permanent: true,
       },
       {
-        source: '/articles/whats-new-in-expo-sdk-50',
-        destination: '/',
-        permanent: true,
+      source: '/articles/whats-new-in-expo-sdk-50',
+      destination: '/articles',
+      permanent: true,
       },
       {
-        source: '/articles/markdown-resume',
-        destination: '/',
-        permanent: true,
+      source: '/articles/markdown-resume',
+      destination: '/articles',
+      permanent: true,
+      },
+      {
+      source: '/articles/mitigating-risk-and-reducing-delays-in-ai-and-software-development-projects',
+      destination: '/articles',
+      permanent: true,
       },
     ];
   },

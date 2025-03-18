b10d13b1c34d1b16121c28004d13c1ee7b108515
Anthony Coffey <coffey.j.anthony@gmail.com>
2025-03-17 20:15:21 -0500

    ...

--- app/og/route.tsx
+++ app/og/route.tsx
@@ -8,9 +8,9 @@ export function GET(request: Request) {
 
   return new ImageResponse(
     (
-      <div tw="flex flex-col w-full h-full items-center justify-center bg-white">
+      <div tw="flex flex-col w-full h-full items-center justify-center bg-black">
         <div tw="flex flex-col md:flex-row w-full py-12 px-4 md:items-center justify-between p-8">
-          <h2 tw="flex flex-col text-4xl font-bold tracking-tight text-left">
+          <h2 tw="flex flex-col text-4xl font-bold tracking-tight text-left text-white">
             {title}
           </h2>
         </div>

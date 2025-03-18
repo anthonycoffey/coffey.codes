847d3a51650b2dbe710fce5f58583fca15444d3f
Anthony Coffey <coffey.j.anthony@gmail.com>
2025-03-17 20:15:35 -0500

    ...

--- app/og/route.tsx
+++ app/og/route.tsx
@@ -10,7 +10,7 @@ export function GET(request: Request) {
     (
       <div tw="flex flex-col w-full h-full items-center justify-center bg-black">
         <div tw="flex flex-col md:flex-row w-full py-12 px-4 md:items-center justify-between p-8">
-          <h2 tw="flex flex-col text-4xl font-bold tracking-tight text-left text-white">
+          <h2 tw="flex flex-col text-4xl font-bold tracking-tight text-center text-white">
             {title}
           </h2>
         </div>

5e930cf43f5a62f8c4a3058664390e7a27f3f0df
Anthony Coffey <coffey.j.anthony@gmail.com>
2025-03-25 02:35:19 -0500

    updating imports

--- app/articles/utils.ts
+++ app/articles/utils.ts
@@ -250,42 +250,6 @@ export function getAllCategories() {
   return Array.from(allCategories).sort();
 }
 
-export function formatDate(date: string, includeRelative = false) {
-  let currentDate = new Date();
-  if (!date.includes('T')) {
-    date = `${date}T00:00:00`;
-  }
-  let targetDate = new Date(date);
-
-  let yearsAgo = currentDate.getFullYear() - targetDate.getFullYear();
-  let monthsAgo = currentDate.getMonth() - targetDate.getMonth();
-  let daysAgo = currentDate.getDate() - targetDate.getDate();
-
-  let formattedDate = '';
-
-  if (yearsAgo > 0) {
-    formattedDate = `${yearsAgo}y ago`;
-  } else if (monthsAgo > 0) {
-    formattedDate = `${monthsAgo}mo ago`;
-  } else if (daysAgo > 0) {
-    formattedDate = `${daysAgo}d ago`;
-  } else {
-    formattedDate = 'Today';
-  }
-
-  let fullDate = targetDate.toLocaleString('en-us', {
-    month: 'short',
-    day: 'numeric',
-    year: 'numeric',
-  });
-
-  if (!includeRelative) {
-    return fullDate;
-  }
-
-  return `${fullDate} (${formattedDate})`;
-}
-
 export function capitalizeWords(text: string): string {
   return text
     .split(' ')

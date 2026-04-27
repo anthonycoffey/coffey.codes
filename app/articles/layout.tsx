9bd0d4fef43be512a7eb590166d07288e77fe535
Anthony Coffey <coffey.j.anthony@gmail.com>
2026-04-26 22:18:15 -0500

    fix: padding and margin in article header / breadcrumbs

--- app/articles/layout.tsx
+++ app/articles/layout.tsx
@@ -4,7 +4,7 @@ export default function BlogLayout({
   children: React.ReactNode;
 }) {
   return (
-    <div className="font-sans text-c-text leading-relaxed mx-auto max-w-4xl px-4 py-8 md:pb-16 min-h-[900px]">
+    <div className="font-sans text-c-text leading-relaxed mx-auto max-w-4xl px-4 md:pb-16 min-h-[900px]">
       {children}
     </div>
   );
--- components/Breadcrumbs.tsx
+++ components/Breadcrumbs.tsx
@@ -13,9 +13,12 @@ export default function Breadcrumbs({ title }: BreadcrumbsProps) {
 
   return (
     // Style container border and text color
-    <nav className="flex items-center text-sm my-4 border-b pb-6 mb-6 border-gray-300 dark:border-neutral-700 text-gray-600 dark:text-gray-400">
+    <nav className="flex items-center text-sm my-4 border-b pb-4 mb-6 border-gray-300 dark:border-neutral-700 text-gray-600 dark:text-gray-400">
       {/* Style Home link */}
-      <Link href="/" className="flex items-center hover:underline dark:hover:text-gray-200">
+      <Link
+        href="/"
+        className="flex items-center hover:underline dark:hover:text-gray-200"
+      >
         <HomeIcon className="h-4 w-4 mr-1" />
         <span>Home</span>
       </Link>
@@ -24,7 +27,10 @@ export default function Breadcrumbs({ title }: BreadcrumbsProps) {
       <span className="mx-2">•</span>
 
       {/* Style Articles link */}
-      <Link href="/articles" className="hover:underline dark:hover:text-gray-200">
+      <Link
+        href="/articles"
+        className="hover:underline dark:hover:text-gray-200"
+      >
         Articles
       </Link>
 
@@ -33,7 +39,10 @@ export default function Breadcrumbs({ title }: BreadcrumbsProps) {
           {/* Style separator */}
           <span className="mx-2">•</span>
           {/* Style final non-link title */}
-          <span className="truncate max-w-xs md:max-w-sm text-gray-800 dark:text-gray-200" title={title}>
+          <span
+            className="truncate text-gray-800 dark:text-gray-200"
+            title={title}
+          >
             {title || pathname.split('/').pop()}
           </span>
         </>
